4d1a297d6631d799c88261488762f2aae012948e
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2025-09-25 14:55:07 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@ant-design/charts', 'antd'],
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-      },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          charts: ['@ant-design/charts'],
+          supabase: ['@supabase/supabase-js'],
+          antd: ['antd']
+        }
+      }
     },
-  },
-});
+    chunkSizeWarningLimit: 1000
+  }
+})
